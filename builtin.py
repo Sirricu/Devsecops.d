38830792d213096e3ce614e2dfcf8b1b02dca456
Jordy Awas <sirri>
2022-02-20 22:29:30 -0500

    added builtin and keywords

---
+++ builtin.py
@@ -0,0 +1,14 @@
+zip
+The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together etc.
+
+If the passed iterators have different lengths, the iterator with the least items decides the length of the new iterator.
+
+Super
+The super() function is used to give access to methods and properties of a parent or sibling class.
+
+The super() function returns an object that represents the parent class.
+
+Sorted
+The sorted() function returns a sorted list of the specified iterable object.
+
+You can specify ascending or descending order. Strings are sorted alphabetically, and numbers are sorted numerically.
