474272b1de2b7a9172d398f64600319ea51cd890
Jordy Awas <sirri>
2022-03-04 00:37:47 -0500

    day21 question of the day

---
+++ qeusOfDay21st.py
@@ -0,0 +1,19 @@
+#what is the output of the following code
+Output: 30
+#What is the output of the following code?
+var = "James" * 2  * 3
+print(var)
+Output:JamesJamesJamesJamesJamesJames
+#what is the output of the following code
+x = 36 / 4 * (3 +  2) * 4 + 2
+print(x)
+Output: 182.00
+#What is the output of the following code
+var1 = 1
+var2 = 2
+var3 = "3"
+print(var + var2 + var3)
+#what is the output of the following code?
+p, q, r = 10, 20 ,30
+print(p, q, r)
+Output:p, q, r = 10, 20 ,30
