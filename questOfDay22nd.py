e5bd7637219e4a94d9768b59b940cce7022e54a8
Jordy Awas <sirri>
2022-03-04 00:49:44 -0500

    Question of day 22

---
+++ questOfDay22nd.py
@@ -0,0 +1,19 @@
+#what is the maximum length of a python idntifier?
+In Python, the highest possible length of an identifier is 79 characters
+#which of the following is an invalid statement
+num = 1000000
+x y z = 1 2 3  # This is invalid
+
+ Python variable names:
+var1.3
+home_address
+route66
+Age
+4square
+
+#All keywords in Python are in 
+Answer : non of the mentioned
+
+#Which of the following are Python reserved words (keywords):
+
+class, None, goto
